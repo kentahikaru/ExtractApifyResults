2a539da26d7bc7026f631e7acc770f7935e2eaad
kentahikaru <quazarstar@gmail.com>
2021-05-09 22:58:15 +0200

    NLog logging

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 
 namespace ExtractApifyResults
 {
@@ -9,12 +12,20 @@ namespace ExtractApifyResults
     {
         static async Task Main(string[] args)
         {
-            await Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(logging => {
+            var config = new ConfigurationBuilder().Build();
 
-            })
+            var logger = LogManager.Setup()
+                                   .SetupExtensions(ext => ext.RegisterConfigSettings(config))
+                                   .GetCurrentClassLogger();
+
+
+            await Host.CreateDefaultBuilder(args)
             .ConfigureServices((hostContext, services) => {
                 services.AddHostedService<ConsoleHostedService>();
+            })
+            .ConfigureLogging(logging => {
+                logging.ClearProviders();
+                logging.AddNLog(config);
             })
             .RunConsoleAsync();
         }
